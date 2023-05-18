a23ba6d8467d0f8ee9461965f3f94ce23a84ebcc
AylenEspinoza <132311769+AylenEspinoza@users.noreply.github.com>
2023-05-18 09:27:24 +0000

    🚩Enrutador

--- app.js
+++ app.js
@@ -1,61 +1,22 @@
-// Importando expressjs
+// Importando Express
 import express from 'express';
-// Crear una instancia de express
-const app = express(); // (req, res)=>{ UN MONTO DE CODIGO }
 
-app.use(express.urlencoded({extended: true}));
+// Importando el enrutador
+import adminRouter from './routes/admin.route.js';
+import shopRouter from './routes/shop.route.js';
 
-// Registrar nuestro primer middleware
-app.use((req, res, next ) => {
-  console.log("⭐ Ejecutando el Middleware 1");
-  // Invocando al siguiente middleware
-  next();
- });
- app.use((req, res, next) => {
-  console.log(`🪄 ${req.method} - ${req.url}`);
-  next();
- });
- app.use('/about', (req, res)=>{
-  res.send(`
-  <h1 style="color: teal">About...</h1>
-  <p style="color: #555"> Esta pagina creada para aprender 
-  desarrollo web en Fullstack con JS</p>
-  `);
- });
+// Creando la instancia de express
+// que basicamente es un middleware
+const app = express();
 
- //GET SED-PRODUCT
-app.get('/add-product', (req, res,next)=>{
-  console.log("👣SIRVIENDO EL FORMULARIO");
+// Se registra el middleware del body-parser
+app.use(express.urlencoded({ extended: true }));
 
-    //sirviendo el formulario 
-    res.send(`
-    <form action="/add-product" method="POST">
-    <label for="title">TITLE</label>
-    <input id="title" type="text" name="title">
-    <label for="title">DESCRIPTION</label>
-    <input id="description" type="text" name="description">
-    <button type="submit">Add product</button>
-    </from>
-    `);
-});
+// Se agrega ruta de administrador
+app.use(adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
 
-//POST /ADD-PRODUCT
-app.post('/add-product', (req, res)=>{
-  //realizando extraccion de los datos en la peticion 
-  for(const prop in req.body){
-    console.log(`PROP: ${prop} : ${req.body[prop]}`);
-  }
-  //redireccionamiento
-  res.redirect('/');
-});
-
- app.use((req, res) => {
-  console.log("⭐ Respondiendo al cliente");
-  res.send(`
-  <h1>Bienvenido a express</h1>
-  <p>Este es mi software</p>
-  `);
- });
 // Creando el servidor
 // const server = http.createServer(app);
 // Definir puertos
